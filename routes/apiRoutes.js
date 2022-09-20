944a939d22318e8508d17c51e24b5bd86c00e1c8
Merkisha Wilkinson <merkisha94@gmail.com>
2022-09-19 22:37:45 -0400

    updated apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,45 @@
-const router = require("express").Router();
-const store = require("../db/store");
+const noteTaking = require("../db/store");
+
+module.exports = app => {
+
+  app.get('/api/notes', (req, res) => {
+    noteTaking
+      .getAll()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).end();
+      });
+  })
+
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    const randomNum = Math.floor((Math.random()*1000000) + 1);
+    newNote.id = randomNum;
+
+    noteTaking
+      .push(newNote)
+      .then(() => {
+        res.json(newNote);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).end();
+      }); 
+  })
+
+  app.get('/api/notes/:id', (req, res) => {
+    const chosen = req.params.id;
+    noteTaking
+      .getAll()
+      .then(data => {
+        res.json(data[chosen]);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(500).end();
+      })
+  })
